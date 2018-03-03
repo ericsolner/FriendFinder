3b38588c7fb0d3e18ca8af7ce3902573e0f0c2d9
Eric Solner <ericsolner2018@u.northwestern.edu>
2018-03-03 09:40:55 -0600

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+
+var friendsArray = [
+	{
+		name: 'Jordan',
+		photo: 'https://cdn.nba.net/nba-drupal-prod/styles/landscape/http/i2.cdn.turner.com/nba/nba/dam/assets/160530160053-michael-jordan-1992.1280x720.jpg?itok=uEdJmKYH',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lebron',
+		photo: 'http://images.performgroup.com/di/library/omnisport/6/27/lebron-james_187vgg1oerbe319r8rsadexf7r.jpg?t=597392865&w=960&quality=70',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Prison',
+		photo: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-07/22/18/campaign_images/buzzfeed-prod-fastlane01/michael-scott-quotes-that-are-still-hilarious-2-10277-1469225647-3_dblbig.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Halpert',
+		photo: 'https://nypdecider.files.wordpress.com/2017/11/jim-halpert.jpg?quality=90&strip=all&w=646&h=431&crop=1',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Rashida',
+		photo: 'https://www.wired.com/wp-content/uploads/2015/06/rashida_opener_background2.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Kanye West',
+		photo: 'https://static.highsnobiety.com/wp-content/uploads/2017/02/23163930/kanye-west-donda-cosmetics-makeup-1-480x320.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'James Franco',
+		photo: 'https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/images/gettyimages-598975728.jpg?itok=1gplxlUq&c=e21e60e7b76b46dcdfcb26610449e7d3',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Aaron Rodgers',
+		photo: 'https://nesncom.files.wordpress.com/2018/01/aaron-rodgers_opt.jpg?w=640',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Chris Farren',
+		photo: 'https://pbs.twimg.com/media/Cwgpo7tVIAAl9-K.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Shrek',
+		photo: 'https://images-production.global.ssl.fastly.net/uploads/photos/file/97951/shrek-fun-facts-1.jpg?auto=compress&crop=top&fit=clip&h=500&q=55&w=698',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
